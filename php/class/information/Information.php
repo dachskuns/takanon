c1bd014de1f290c4d6ec5adaa22e293156653349
student <student@PC634S26.asost.local>
2015-06-16 15:10:54 +0900

    前岳さっさ来い

--- php/class/information/Information.php
+++ php/class/information/Information.php
@@ -1,14 +1,78 @@
 <?php
 class Information {
-
 	private $mcode;
+	private $result;
+	private $result_flag;
+	private $column;
+	private $table;
+	private $conditions;
+	private $value;
+	private $sql;
+	private $link;
+	private $db_selected;
 
+	// ここからGetter
 	public function getMcode() {
 		return $this->mcode;
 	}
+	public function getResult() {
+		return $this->result;
+	}
+	public function getResult_flag() {
+		return $this->result_flag;
+	}
+	public function getColumn() {
+		return $this->column;
+	}
+	public function getTable() {
+		return $this->table;
+	}
+	public function getConditions() {
+		return $this->conditions;
+	}
+	public function getValue() {
+		return $this->value;
+	}
+	public function getSql() {
+		return $this->sql;
+	}
+	public function getLink() {
+		return $this->link;
+	}
+	public function getDb_selected() {
+		return $this->db_selected;
+	}
 
-	public function setMcode() {
-		$this->mcode = $mccode;
+	// ここからSetter
+	public function setMcode($mcode) {
+		$this->mcode = $mcode;
+	}
+	public function setResult($result) {
+		$this->result = $result;
+	}
+	public function setResult_flag($result_flag) {
+		$this->result_flag = $result_flag;
+	}
+	public function setColumn($column) {
+		$this->column = $column;
+	}
+	public function setTable($table) {
+		$this->table = $table;
+	}
+	public function setConditions($conditions) {
+		$this->conditions = $conditions;
+	}
+	public function setValue($value) {
+		$this->value = $value;
+	}
+	public function setSql($sql) {
+		$this->sql = $sql;
+	}
+	public function setLink($link) {
+		$this->link = $link;
+	}
+	public function setDb_selected($db_selected) {
+		$this->db_selected = $db_selected;
 	}
 }
 ?>
--- php/class/super/Preparation.php
+++ php/class/super/Preparation.php
@@ -1,12 +1,33 @@
 <?php
+	require 'Information.php';
+
+
 	class DBOpen{
-		public function Db_Open($dbname, $mode) {
-			
+		public function Db_Open($db_name) {
+			$info = new Information();
+			$info->setLink(mysql_connect('127.0.0.1', 'root','root'));
+			if(!$info->getLink()){
+				die('接続失敗です。'.mysql_error());
+			}
+
+			$info->setDb_selected($db_name, $info->getLink());
+			if($info->getDb_selected()){
+				die('データベース選択失敗です。'.mysql_error());
+			}
+			return $info;
 		}
 	}
 
 
-	class SqlMolding {
+	class SqlMolding extends DBOpen{
+		public function sql_molding() {
+			$info = parent::Db_Open('takanon');
+			$sql = $info->getSql();
+			if($sql === 'select'){
+				$column = $info->
+
+			}
+		}
 
 	}
 ?>
